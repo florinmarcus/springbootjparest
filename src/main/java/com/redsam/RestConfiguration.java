665aef5cd0fc5e5d52c9d3b1f9cf57327a3a11d2
florinmarcus <florin.marcus@gmail.com>
2018-02-06 10:11:39 +0000

    regions rest customization - CORS security and misc

--- src/main/java/com/redsam/RestConfiguration.java
+++ src/main/java/com/redsam/RestConfiguration.java
@@ -3,21 +3,51 @@ package com.redsam;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import com.redsam.model.*;
+
 @Configuration
-public class RestConfiguration {
+public class RestConfiguration  {
 
     @Bean
     public FilterRegistrationBean corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
         source.registerCorsConfiguration("/**", config);
+        config.addAllowedMethod(HttpMethod.GET);
+        config.addAllowedMethod(HttpMethod.POST);
+        config.addAllowedMethod(HttpMethod.PUT);
+        config.addAllowedMethod(HttpMethod.DELETE);
+        config.addAllowedMethod(HttpMethod.OPTIONS);
+        System.out.println("config.getAllowedMethods():::: ");
         FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
         bean.setOrder(0);
-        System.out.println("cors filter.........");
         return bean;
+    }
+    
+    @Bean
+    public RepositoryRestConfigurer repositoryRestConfigurer() {
+
+      return new RepositoryRestConfigurerAdapter() {
+
+        @Override
+        public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+          config.exposeIdsFor(Regions.class);
+          config.exposeIdsFor(Locations.class);
+          config.exposeIdsFor(Jobs.class);
+          config.exposeIdsFor(Employees.class);
+          config.exposeIdsFor(Departments.class);
+          config.exposeIdsFor(Customer.class);
+          config.exposeIdsFor(Countries.class);
+        }
+      };
     }
 }
