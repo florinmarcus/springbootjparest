183372dde12528b2185bb691f68df06f60cdd849
florinmarcus <florin.marcus@gmail.com>
2018-03-04 19:31:24 +0000

    Departments DTO objects added

---
+++ src/main/java/com/redsam/dto/DepartmentsDTOImpl.java
@@ -0,0 +1,18 @@
+package com.redsam.dto;
+
+import org.springframework.data.rest.core.config.Projection;
+
+import com.redsam.model.Departments;
+import com.redsam.model.Employees;
+import com.redsam.model.Locations;
+
+public classs DepartmentsDTOImpl {
+	
+	public Long getDepartmentId();
+
+    public String getDepartmentName();
+
+	
+	
+
+}
